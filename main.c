904a7255e8d891d146bc9edd04d24ab58c8e80ea
Gabs.exe Failed to Load <112909127+GabrielRugila@users.noreply.github.com>
2022-11-07 10:12:07 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 struct boletim{
-    float notas[2][2]; // 2t, 2p // valido: t0.0 a 2.0 , p0.0 a 3.0
+    float notas[2][2];
     float media;
-    int passou; // 0 = reprovado (media < 6) || 1 = aprovado // boolean seria +memory efficient?
+    int passou;
 };
 
 struct alunos{
     char nome[50];
     int serie;
-    struct boletim b; //alunos -> aluno -> [i] -> b -> (notas, media, passou)
+    struct boletim b;
 };
 
 int lerQtdAlunos();
@@ -25,14 +25,17 @@ int main(){
     int qtdAlunos = lerQtdAlunos();
     struct alunos aluno[qtdAlunos];
 
-    for(i = 0; i < qtdAlunos; i++){ //implementar l14 a l19
-        //função lerQtdAlunos() deveria ser construida fora do for
+    for(i = 0; i < qtdAlunos; i++){
+
+        printf("\nAluno numero %d", i+1);
 
         aluno[i] = lerAlunos(i);
-        lerNotas();
-        calcularMedia(aluno[i].b);
-        validarPassou(aluno[i].b);
-        mostraAlunos(aluno[i]);
+
+        aluno[i].b = lerNotas();
+
+        aluno[i].b.media = calcularMedia(aluno[i].b);
+
+        aluno[i].b.passou = validarPassou(aluno[i].b);
 
     }
     for(i=0; i<qtdAlunos; i++){
@@ -43,16 +46,16 @@ int main(){
 }
 
 int lerQtdAlunos(){
-    int quantidadeDAlunos;
+    int nAlunos;
     printf("Insira a quantidade de alunos a serem cadastrados: ");
-    scanf("%d", &quantidadeDAlunos);
-    return quantidadeDAlunos;
+    scanf("%d", &nAlunos);
+    return nAlunos;
 };
 
 struct alunos lerAlunos(int numAluno){
     struct alunos aluno;
     printf("\n%d - Insira nome do aluno: ", numAluno+1); //i = n do aluno atual
-    scanf("%s",aluno.nome);
+    scanf("%s",&aluno.nome);
 
     printf("\n%d - Insira a serie do aluno: ", numAluno+1);
     scanf("%d", &aluno.serie);
@@ -63,59 +66,50 @@ struct alunos lerAlunos(int numAluno){
 struct boletim lerNotas() {
     struct alunos aluno;
     for (int lin = 0; lin < 2; lin++) {
-        printf("\nNotas de trabalho devem ser de 0 a 2 e notas de prova de 0 a 3");
 
         for (int col = 0; col < 2; col++) {
-            do {
             if (lin == 0) {
-                printf("\nInsira a nota do trabalho %d do aluno: ", col+1);
+                do {
+                    printf("\nNotas de trabalho devem ser de 0 a 2");
+                    printf("\nInsira a nota do trabalho %d do aluno: ", col + 1);
+                        scanf("%f", &aluno.b.notas[0][col]);
+                } while (aluno.b.notas[0][col] > 2 || aluno.b.notas[0][col] < 0);
             } else {
-                printf("\nInsira a nota %d de prova: ", col+1);
+                do {
+                    printf("\nNotas de prova devem ser de 0 a 3");
+                    printf("\nInsira a nota %d de prova: ", col + 1);
+                    scanf("%f", &aluno.b.notas[1][col]);
+                } while (aluno.b.notas[1][col] > 3 || aluno.b.notas[1][col] < 0);
             }
-                float notaTemp = 0;
-                scanf("%f", &notaTemp);
-                aluno.b.notas[lin][col] = notaTemp;
-                printf("\n%f", notaTemp);
-                printf("\n%f", aluno.b.notas[lin][col]);
-            } while (aluno.b.notas[0][col] > 2 || aluno.b.notas[1][col] > 3 || aluno.b.notas[lin][col] <= 0);
-//            return aluno.b.notas[lin][col];
         }
     }
+    return aluno.b;
 };
 
 float calcularMedia(struct boletim b){
-    struct alunos aluno;
-    float mediaTemp = 0;
+
     for (int lin = 0; lin < 2; lin++){
         for(int col = 0; col < 2; col++){
-            mediaTemp += aluno.b.notas[lin][col];
-            printf("%f", mediaTemp);
-            aluno.b.media += mediaTemp;
-            printf("%f", aluno.b.media);
+            b.media += b.notas[lin][col];
         }
     }
-return b.media;
+    return b.media;
 };
 
 int validarPassou(struct boletim b){
-    struct alunos aluno;
-
-    float mediaTemp = b.media;
-    if(mediaTemp >= 6){
-        aluno.b.passou = 1;
-        return aluno.b.passou;
+    if(b.media >= 6){
+        return b.passou = 1;
     } else {
-        aluno.b.passou = 0;
-        return aluno.b.passou;
+        return b.passou = 0;
     }
 };
 
 void mostraAlunos(struct alunos aluno){
-        printf("\n\t %s", aluno.nome);
-        printf("\n\t %d", aluno.serie);
-        if(aluno.b.passou == 1){
-            printf("\nO aluno PASSOU com media %.1f", aluno.b.media);
-        } else {
-            printf("\nO aluno REPROVOU com media %.1f", aluno.b.media);
-        }
+    printf("\n\t %s", aluno.nome);
+    printf("\n\t %d", aluno.serie);
+    if(aluno.b.passou == 1){
+        printf("\nO aluno PASSOU com media %.1f", aluno.b.media);
+    } else {
+        printf("\nO aluno REPROVOU com media %.1f", aluno.b.media);
+    }
 };
